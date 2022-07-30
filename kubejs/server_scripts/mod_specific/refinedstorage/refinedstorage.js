8a312c734b6995ba3a720d19580a217710d52d7d
zackpackcraft <110276526+zackpackcraft@users.noreply.github.com>
2022-07-30 01:49:59 -0400

    upload

---
+++ kubejs/server_scripts/mod_specific/refinedstorage/refinedstorage.js
@@ -0,0 +1,156 @@
+ onEvent('recipes', e => {
+	e.shaped('extrastorage:iron_crafter', ['B B', 'PCP', 'B B'], {
+    B: '#forge:storage_blocks/iron',
+    P: 'refinedstorage:improved_processor',
+    C: '#refinedstorage:crafter'
+  })
+  
+  //Cable Tiers
+  function caTier(tier, corners, processor, cables) {
+    Utils.listOf(['importer', 'exporter', 'constructor', 'destructor', 'disk_manipulator', 'requester']).forEach(caType => {
+      e.shaped(`cabletiers:${tier}_${caType}`, [`a a`, `bcb`, `a a`], {
+        a: corners,
+        b: processor,
+        c: `${(caType == 'requester' && tier == 'elite') ? 'rsrequestify:' : cables}${caType}`
+      })
+    })
+  }
+
+  e.remove({ mod: 'cabletiers' })
+  
+  caTier(`elite`, `#forge:storage_blocks/iron`, `refinedstorage:improved_processor`, `refinedstorage:`)
+  caTier(`ultra`, `#forge:storage_blocks/diamond`, `refinedstorage:advanced_processor`, `cabletiers:elite_`)
+  caTier(`creative`, `#forge:storage_blocks/netherite`, `extradisks:withering_processor`, `cabletiers:ultra_`)
+  
+  //Creative Crafter
+  e.shaped(`creativecrafter:creative_crafter`, [`BUB`, `PCP`, `BUB`], {
+    B: `#forge:storage_blocks/netherite`,
+    P: `extradisks:withering_processor`,
+    C: `extrastorage:netherite_crafter`,
+    U: `#forge:ingots/unobtainium`
+  }).id(`kubejs:creativecrafter/creative_crafter`)
+  
+  //gold-netherite crafter temp till mod updates with proper tags
+  modifyShaped(e, 'extrastorage:gold_crafter', 1, ['BSB', 'PCP', 'B B'], {
+    S: '#forge:chests',
+    B: '#forge:storage_blocks/gold',
+    P: 'extrastorage:neural_processor',
+    C: 'extrastorage:iron_crafter'
+  })
+  modifyShaped(e, 'extrastorage:diamond_crafter', 1, ['BSB', 'PCP', 'B B'], {
+    S: '#forge:chests',
+    B: '#forge:storage_blocks/diamond',
+    P: 'extrastorage:neural_processor',
+    C: 'extrastorage:gold_crafter'
+  })
+  modifyShaped(e, 'extrastorage:netherite_crafter', 1, ['BSB', 'PCP', 'B B'], {
+    S: '#forge:chests',
+    B: '#forge:storage_blocks/netherite',
+    P: 'extrastorage:neural_processor',
+    C: 'extrastorage:diamond_crafter'
+  })
+  e.shaped('extradisks:1024k_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:256k_storage_part',
+    b: '#forge:dusts/redstone',
+    n: 'refinedstorage:quartz_enriched_iron'
+  }).id('kubejs:1024k_storage_part')
+  e.shaped('refinedstorage:4096k_fluid_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'refinedstorage:1024k_fluid_storage_part',
+    b: 'minecraft:bucket',
+    n: '#forge:nuggets/allthemodium'
+  })
+  e.shaped('extradisks:4096k_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:1024k_storage_part',
+    b: '#forge:dusts/redstone',
+    n: '#forge:nuggets/allthemodium'
+  })
+  e.shaped('extradisks:16384k_fluid_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'refinedstorage:4096k_fluid_storage_part',
+    b: 'minecraft:bucket',
+    n: '#forge:ingots/allthemodium'
+  })
+  e.shaped('extradisks:16384k_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:4096k_storage_part',
+    b: '#forge:dusts/redstone',
+    n: '#forge:ingots/allthemodium'
+  })
+  e.shaped('extradisks:65536k_fluid_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:16384k_fluid_storage_part',
+    b: 'minecraft:bucket',
+    n: '#forge:ingots/allthemodium'
+  })
+  e.shaped('extradisks:65536k_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:16384k_storage_part',
+    b: '#forge:dusts/redstone',
+    n: '#forge:ingots/allthemodium'
+  })
+  e.shaped('extradisks:262144k_fluid_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:65536k_fluid_storage_part',
+    b: 'minecraft:bucket',
+    n: '#forge:ingots/vibranium'
+  })
+  e.shaped('extradisks:262144k_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:65536k_storage_part',
+    b: '#forge:dusts/redstone',
+    n: '#forge:ingots/vibranium'
+  })
+  e.shaped('extradisks:1048576k_fluid_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:262144k_fluid_storage_part',
+    b: 'minecraft:bucket',
+    n: '#forge:ingots/unobtainium'
+  })
+  e.shaped('extradisks:1048576k_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:262144k_storage_part',
+    b: '#forge:dusts/redstone',
+    n: '#forge:ingots/unobtainium'
+  })
+  e.shaped('extradisks:infinite_fluid_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:1048576k_fluid_storage_part',
+    b: 'minecraft:bucket',
+    n: '#forge:ingots/unobtainium'
+  })
+  e.shaped('extradisks:infinite_storage_part', ['ana', 'fbf', 'afa'], {
+    a: 'refinedstorage:advanced_processor',
+    f: 'extradisks:1048576k_storage_part',
+    b: '#forge:dusts/redstone',
+    n: '#forge:ingots/unobtainium'
+  })
+  removeRecipeByID(e, [
+    'extradisks:part/1024k_storage_part',
+    'refinedstorage:part/4096k_fluid_storage_part',
+    'extradisks:part/4096k_storage_part',
+    'extradisks:part/16384k_fluid_storage_part',
+    'extradisks:part/16384k_storage_part',
+    'extradisks:part/65536k_fluid_storage_part',
+    'extradisks:part/65536k_storage_part',
+    'extradisks:part/262144k_fluid_storage_part',
+    'extradisks:part/262144k_storage_part',
+    'extradisks:part/1048576k_fluid_storage_part',
+    'extradisks:part/1048576k_storage_part',
+    'extradisks:part/infinite_fluid_storage_part',
+    'extradisks:part/infinite_storage_part',
+    /^extrastorage:(?:part|disk|storage_block)\/.+/,
+	'creativecrafter:creative_crafter',
+    'creativewirelesstransmitter:creative_wireless_transmitter',
+    'extrastorage:iron_crafter'
+  ])
+  e.remove({id: 'rsinfinitybooster:infinity_card'})
+  e.shaped('rsinfinitybooster:infinity_card', ['EBE', 'BUB', 'NNN'], {
+   U: '#forge:ingots/unobtainium',
+   B: 'refinedstorage:range_upgrade',
+   E: 'alltheores:enderium_plate',
+   N: 'minecraft:netherite_ingot'
+  })
+})
---
+++ kubejs/server_scripts/tags.js
@@ -0,0 +1,57 @@
+//priority: 1000
+onEvent('tags.blocks', e => {
+  e.add('allthemodium:other_te_whitelist', ['mekanism:teleporter', 'waystones:waystone', 'waystones:mossy_waystone', 'waystones:sandy_waystone', '#waystones:sharestones'])
+  e.add('ae2:blacklisted/spatial','#forge:relocation_not_supported')
+})
+
+onEvent('tags.items', e => {
+  e.add('misctags:farmland/tier6', ['mysticalagradditions:insanium_farmland'])
+  e.add('misctags:farmland/tier5', ['mysticalagriculture:supremium_farmland', '#misctags:farmland/tier6'])
+  e.add('misctags:farmland/tier4', ['mysticalagriculture:imperium_farmland', '#misctags:farmland/tier5'])
+  e.add('misctags:farmland/tier3', ['mysticalagriculture:tertium_farmland', '#misctags:farmland/tier4'])
+  e.add('misctags:farmland/tier2', ['mysticalagriculture:prudentium_farmland', '#misctags:farmland/tier3'])
+  e.add('misctags:farmland/tier1', ['mysticalagriculture:inferium_farmland', '#misctags:farmland/tier2'])
+  e.add('misctags:immersive_engineering_hammer', 'immersiveengineering:hammer')
+  e.add('misctags:immersive_engineering_wirecutter', 'immersiveengineering:wirecutter')
+
+  e.add('forge:ores/quartz', ['byg:blue_nether_quartz_ore', 'byg:brimstone_nether_quartz_ore'])
+  e.add('forge:ores/gold', ['byg:blue_nether_gold_ore', 'byg:brimstone_nether_gold_ore'])
+  e.add('forge:ores/inferium',['mysticalagradditions:nether_inferium_ore','mysticalagradditions:end_inferium_ore'])
+
+  e.add('forge:storage_blocks/raw_crimson_iron', 'silentgear:raw_crimson_iron_block');
+  e.add('forge:raw_ores/crimson_iron', 'silentgear:raw_crimson_iron');
+  e.add('forge:raw_materials/crimson_iron', 'silentgear:raw_crimson_iron');
+  e.add('forge:dusts/crimson_iron', 'silentgear:crimson_iron_dust');
+  e.add('forge:storage_blocks/raw_azure_silver', 'silentgear:raw_azure_silver_block');
+  e.add('forge:raw_ores/azure_silver', 'silentgear:raw_azure_silver');
+  e.add('forge:raw_materials/azure_silver', 'silentgear:raw_azure_silver');
+  e.add('forge:dusts/azure_silver', 'silentgear:azure_silver_dust');
+  e.add('forge:raw_ores/cobalt', 'tconstruct:raw_cobalt');
+  e.add('forge:dusts/cobalt', 'kubejs:cobalt_dust');
+
+  e.add('forge:raw_ores/iridium', 'ftbic:iridium_chunk');
+  e.remove('forge:silicon', 'ftbic:silicon')
+
+  e.add('forge:melons','minecraft:melon_slice')
+  e.add('forbidden_arcanus:modifier/eternal_incompatible',['#alltheores:ore_hammers','@ftbic','#tconstruct:modifiable','minecraft:nether_star','mythicbotany:faded_nether_star'])
+ 
+  // fix raw block crafting for other mods
+  e.add('forge:raw_ores/zinc', 'create:raw_zinc')
+  mekanismMetals.forEach(metal => e.add(`forge:raw_ores/${metal}`, `mekanism:raw_${metal}`))
+  immersiveMetals.forEach(metal => e.add(`forge:raw_ores/${metal}`, `immersiveengineering:raw_${metal}`))
+
+  atmMetals.forEach(metal => e.add(`forge:storage_blocks/raw_${metal}`, `allthemodium:raw_${metal}_block`));
+  e.add('forge:rods/metal', atoMetals.concat(vanillaMetals, atoAlloys).map(metal => `alltheores:${metal}_rod`));
+  e.add('forge:rods/metal', atmMetals.map(metal => `allthemodium:${metal}_rod`));
+  e.add('forge:rods/all_metal', '#forge:rods/metal');
+
+  e.remove('forge:storage_blocks/copper', 'minecraft:cut_copper')
+})
+onEvent('tags.blocks', e => {
+  e.add('minecraft:climbable', ['minecraft:chain', /additionallanterns:.*_chain/])
+})
+onEvent('tags.entity_types', e => {
+  e.add('mob_grinding_utils:noswab', [/productivebees:.+/, 'artifacts:mimic','botania:doppleganger','allthemodium:piglich',])
+  e.add('pneumaticcraft:vacuum_trap_blacklisted', [/productivebees:.+/, 'artifacts:mimic','botania:doppleganger','allthemodium:piglich',])
+  e.add('industrialforegoing:mob_duplicator_blacklist',[/productivebees:.+/, 'artifacts:mimic','botania:doppleganger','allthemodium:piglich',])
+})
